f25a8b5087334f2ef2de3af30172d08c96ae83f8
Leftfish <bohdan@widla.net>
2024-12-01 10:16:30 +0100

    Add solution to Day 1 (both parts)

---
+++ 01/d01.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+import os
+
+
+def parse_data(data):
+    left, right = [], []
+    for line in data.splitlines():
+        first, second = line.split('   ')
+        left.append(int(first))
+        right.append(int(second))
+    return sorted(left), sorted(right), Counter(right)
+
+
+def sum_distances(left, right):
+    return sum([abs(pair[0] - pair[1]) for pair in zip(left, right)])
+
+
+def calculate_similarity(left, appearances):
+    score = 0
+    for num in left:
+        if num in appearances:
+            score += num * appearances[num]
+    return score
+
+
+DAY = 1
+TEST_DATA = '''3   4
+4   3
+2   5
+1   3
+3   9
+3   3'''
+
+
+print(f'Day {DAY} of Advent of Code!')
+print('Testing...')
+left, right, appearances = parse_data(TEST_DATA)
+print('Sum of distances between lists:', sum_distances(left, right) == 11)
+print('Similarity score:', calculate_similarity(left, appearances) == 31)
+
+input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
+with open(input_path, mode='r', encoding='utf-8') as inp:
+    print('Solution...')
+    data = inp.read()
+    left, right, appearances = parse_data(data)
+    print('Sum of distances between lists:', sum_distances(left, right))
+    print('Similarity score:', calculate_similarity(left, appearances))
---
+++ README.md
@@ -0,0 +1,17 @@
+# Advent of Code 2024
+
+For the seventh time tried to find out how far I can make it in [Advent of Code](https://adventofcode.com/2023/). Results for previous editions:
+* 2018: 9 days
+* 2019: 13 days
+* [2020](https://github.com/Leftfish/Advent-of-Code-2020): 25 days for the first time!
+* [2021](https://github.com/Leftfish/Advent-of-Code-2021): 25 days for the second time!
+* [2022](https://github.com/Leftfish/Advent-of-Code-2022): 25 days for the third time!
+* [2023](https://github.com/Leftfish/Advent-of-Code-2023): 25 days for the third time!
+
+Not only am I (still) not a pro, nor do I have a degree in computer science, but also this is a pretty challenging time for me. So I'm totally not sure how much effort I can devote to AoC this year. But let's try anyone. 
+
+As every year, shoutout to the wonderful [AoC community on reddit](https://www.reddit.com/r/adventofcode/).
+
+Things I **L**earned, **R**evised or **I**mproved at in 2024:
+
+* [Day 1 Python](01/d01.py): reading comprehension (because I tried to solve a much harder puzzle for about 20 minutes...) and collections.Counter (**R**)
