79190a4e213495b7cc0f8b284e60461daf79db77
Leftfish <bohdan@widla.net>
2024-12-02 17:34:06 +0100

    Combine solutions to both parts of Day 2 in one function.

--- 02/d02.py
+++ 02/d02.py
@@ -2,49 +2,33 @@ import os
 
 
 def parse_data(data):
-    levels = []
-    for line in data.splitlines():
-        level = [int(d) for d in line.split()]
-        levels.append(level)
-    return levels
+    return [[int(d) for d in line.split()] for line in data.splitlines()]
 
 
-def check_safety(level):
-    diffs = [level[i+1] - level[i] for i in range(len(level)-1)]
-
-    abs_check = all([3 >= abs(d) >= 1 for d in diffs])
-    sign_check = all([d > 0 for d in diffs]) or all([d < 0 for d in diffs])
-
-    return abs_check and sign_check
-
-
-def check_safety_dampener(level):
-    i = 0
+def check_safety(level, errors_allowed):
     increasing = None
-    candidate = None
+    error_idx = None
 
     # find the index of the first error
-    while i < len(level)-1:
+    for i in range(len(level)-1):
         diff = level[i+1] - level[i]
 
         if increasing is None:
             increasing = diff > 0
 
-        if increasing and (diff > 3 or diff < 1) or \
+        elif increasing and (diff > 3 or diff < 1) or \
             not increasing and (diff > -1 or diff < -3):
-            candidate = i
+            error_idx = i
             break
 
-        i += 1
-
     # if no errors, all good!
-    if candidate is None:
+    if error_idx is None:
         return True
 
-    # remove the number at index, index-1 and index+1, recheck
-    else:
+    # if dampener active: remove the number at index, index-1 and index+1, recheck
+    if errors_allowed:
         updates = []
-        for idx in (candidate-1, candidate, candidate+1):
+        for idx in (error_idx-1, error_idx, error_idx+1):
             if idx < 0 or idx > len(level)-1:
                 continue
             new_level = level[::]
@@ -52,7 +36,7 @@ def check_safety_dampener(level):
             updates.append(new_level)
 
         for updated_level in updates:
-            if check_safety(updated_level):
+            if check_safety(updated_level, errors_allowed=False):
                 return True
 
     return False
@@ -61,11 +45,11 @@ def check_safety_dampener(level):
 def check_levels(levels):
     safe = 0
     safe_dampened = 0
-    
+
     for level in levels:
-        if check_safety(level):
+        if check_safety(level, False):
             safe += 1
-        if check_safety_dampener(level):
+        if check_safety(level, True):
             safe_dampened += 1
 
     return safe, safe_dampened
@@ -82,14 +66,12 @@ TEST_DATA = '''7 6 4 2 1
 
 print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
-levels = parse_data(TEST_DATA)
-safe, safe_damp = check_levels(levels)
+safe, safe_damp = check_levels(parse_data(TEST_DATA))
 print('Safe:', safe == 2, 'With dampener:', safe_damp == 4)
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
 with open(input_path, mode='r', encoding='utf-8') as inp:
     print('Solution...')
     data = inp.read()
-    levels = parse_data(data)
-    safe, safe_damp = check_levels(levels)
+    safe, safe_damp = check_levels(parse_data(data))
     print('Safe:', safe, 'With dampener:', safe_damp)
