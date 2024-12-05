e1b1a9899581270d27d0cef3bd46451dcec47839
Leftfish <bohdan@widla.net>
2024-12-05 11:01:44 +0100

    Refactor Day 5.

--- 05/d05.py
+++ 05/d05.py
@@ -30,15 +30,7 @@ def test_page(page, rules):
     return True
 
 
-def sum_mid(pages, rules):
-    s = 0
-    for page in pages:
-        if test_page(page, rules):
-            s += page[len(page)//2]
-    return s
-
-
-def sum_mid_wrong(pages, rules):
+def sum_mid_pages(pages, rules):
     def comparator(a, b):
         if b in rules[a]:
             return 1
@@ -47,13 +39,17 @@ def sum_mid_wrong(pages, rules):
         else:
             return 0
 
-    s = 0
+    sum_proper, sum_rearranged = 0, 0
     for page in pages:
-        if not test_page(page, rules):
+        # PART 1
+        if test_page(page, rules):
+            sum_proper += page[len(page)//2]
+        # PART 2
+        else: 
             arranged = sorted(page, key=cmp_to_key(comparator))
-            s += arranged[len(arranged)//2]
+            sum_rearranged += arranged[len(arranged)//2]
 
-    return s
+    return sum_proper, sum_rearranged
 
 
 TEST_DATA = '''47|53
@@ -88,12 +84,14 @@ TEST_DATA = '''47|53
 print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
 rules, pages = parse_data(TEST_DATA)
-print('Sum of mid pages in proper:', sum_mid(pages, rules) == 143)
-print('Sum of mid pages in rearranged:', sum_mid_wrong(pages, rules) == 123)
+part_one, part_two = sum_mid_pages(pages, rules)
+print('Sum of mid pages in proper:', part_one == 143)
+print('Sum of mid pages in rearranged:', part_two == 123)
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
 with open(input_path, mode='r', encoding='utf-8') as inp:
     print('Solution...')
     rules, pages = parse_data(inp.read())
-    print('Sum of mid pages in proper:', sum_mid(pages, rules))
-    print('Sum of mid pages in rearranged:', sum_mid_wrong(pages, rules))
+    part_one, part_two = sum_mid_pages(pages, rules)
+    print('Sum of mid pages in proper:', part_one)
+    print('Sum of mid pages in rearranged:', part_two)
