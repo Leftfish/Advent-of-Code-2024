a7a7517148cb1f6a4bf3d5b56a05e8e16d670456
Leftfish <bohdan@widla.net>
2024-12-07 12:20:34 +0100

    Add improved Day 7 solution.

--- 07/d07.py
+++ 07/d07.py
@@ -1,7 +1,7 @@
 import os
 
 from operator import add, mul
-from collections import deque
+from collections import deque # used only in the inefficient version
 
 DAY = 7
 
@@ -20,6 +20,7 @@ def con(a, b):
     return int(str(a) + str(b))
 
 
+''' FIRST ATTEMPT - INEFFICIENT
 def test_equation(equation, ops):
     expected, params = equation
     result, rest = params[0], deque(params[1:])
@@ -45,6 +46,35 @@ def test_equation(equation, ops):
             if res == expected:
                 return expected
     return 0
+'''
+
+
+def test_equation(exp, res, params, part):
+    if res > exp:
+        return False
+    if not params:
+        return res == exp
+    if part == 1:
+        return test_equation(exp, mul(res, params[0]), params.copy()[1:], part=1) or \
+                test_equation(exp, add(res, params[0]), params.copy()[1:], part=1)
+    elif part == 2:
+        return test_equation(exp, mul(res, params[0]), params.copy()[1:], part=2) or \
+                test_equation(exp, add(res, params[0]), params.copy()[1:], part=2) or \
+                test_equation(exp, con(res, params[0]), params.copy()[1:], part=2)
+
+
+def test_all_equations(equations):
+    two_ops = 0
+    three_ops = 0
+    for equation in equations:
+        exp, params = equation
+        res = params.pop(0)
+        if test_equation(exp, res, params, part=1):
+            two_ops += exp
+        if test_equation(exp, res, params, part=2):
+            three_ops += exp
+    return two_ops, three_ops
+
 
 TEST_DATA = '''190: 10 19
 3267: 81 40 27
@@ -60,11 +90,7 @@ TEST_DATA = '''190: 10 19
 print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
 equations = parse_data(TEST_DATA)
-two_ops = 0
-three_ops = 0
-for equation in equations:
-    two_ops += test_equation(equation, (add, mul))
-    three_ops += test_equation(equation, (add, mul, con))
+two_ops, three_ops = test_all_equations(equations)
 print(f'Two ops: {two_ops == 3749}. Three ops: {three_ops == 11387}.')
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
@@ -72,9 +98,5 @@ with open(input_path, mode='r', encoding='utf-8') as inp:
     print('Solution...')
     data = inp.read()
     equations = parse_data(data)
-    two_ops = 0
-    three_ops = 0
-    for equation in equations:
-        two_ops += test_equation(equation, (add, mul))
-        three_ops += test_equation(equation, (add, mul, con))
+    two_ops, three_ops = test_all_equations(equations)
     print(f'Two ops: {two_ops}. Three ops: {three_ops}.')
