befbfc79160ab6003a409ad86b3a6f05426a326e
Leftfish <bohdan@widla.net>
2024-12-25 13:38:53 +0100

    Add solution to Day 24.

--- 24/d24.py
+++ 24/d24.py
@@ -3,6 +3,8 @@ import os
 import re
 from collections import deque, defaultdict
 
+import graphviz
+
 DAY = 24
 
 class Gate:
@@ -36,7 +38,7 @@ class XOR(Gate):
 
 def parse(data):
     wires = {}
-    gates = set()
+    gates = []
     exec_queue = deque()
     wire_to_gate = defaultdict(list)
     
@@ -61,7 +63,7 @@ def parse(data):
             wires[out] = None
 
         new_gate = name_to_type[gate_type](name=i, a=in_left, b=in_right, out=out)
-        gates.add(new_gate)
+        gates.append(new_gate)
 
         wire_to_gate[in_left].append(new_gate)
         wire_to_gate[in_right].append(new_gate)
@@ -81,15 +83,17 @@ def get_z(wires):
     return number, int(number, 2)
 
 
-def run_gates(wires, gates, exec_queue, wire_to_gate):
+def run_gates(wires, exec_queue, wire_to_gate):
     while exec_queue:
         to_exec = exec_queue.popleft()
         target = to_exec.out
         result = to_exec.exec(wires)
+        #print(f'Executing {to_exec}. {result} ---> {target}.')
         wires[target] = result
         for potential_next in wire_to_gate[target]:
             if potential_next not in exec_queue and potential_next.ready(wires):
                 exec_queue.append(potential_next)
+    #print(wires)
     return get_z(wires)[1]
 
 
@@ -144,11 +148,93 @@ tnw OR pbm -> gnj'''
 wires, gates, exec_queue, wire_to_gate = parse(TEST_DATA)
 print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
-print('Number:', run_gates(wires, gates, exec_queue, wire_to_gate) == 2024)
+print('Number:', run_gates(wires, exec_queue, wire_to_gate) == 2024)
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
 with open(input_path, mode='r', encoding='utf-8') as inp:
     print('Solution...')
     data = inp.read()
     wires, gates, exec_queue, wire_to_gate = parse(data)
-    print('Number:', run_gates(wires, gates, exec_queue, wire_to_gate))
+    print('Number:', run_gates(wires, exec_queue, wire_to_gate))
+
+### And now for something completely differnt. A couple of helpers to tinker with the gates
+### and, most importantly, to render the whole blueprint of the adder.
+
+def render():
+    gates_graph = graphviz.Digraph('Day 24', format='png')
+    shapes = {'XOR': 'invhouse', 'AND': 'polygon', 'OR': 'ellipse', 'wire': 'point'}
+
+    for i, gate in enumerate(gates):
+        gate_name = type(gate).__name__
+        gate_id = f'gate {i}'
+        gates_graph.node(gate_id, f'{gate_name} ({i})', shape=shapes[gate_name])
+        a = gate.a
+        b = gate.b
+        o = gate.out
+        gates_graph.node(a, a, shape='point')
+        gates_graph.edge(a, gate_id, label=a)
+        gates_graph.node(b, b, shape='point')
+        gates_graph.edge(b, gate_id, label=b)
+        gates_graph.node(o, o, shape='point')
+        gates_graph.edge(gate_id, o, label=o)
+
+    gates_graph.renderer = 'cairo'
+    gates_graph.render()
+
+def reset(wires):
+    for i in range(46):
+        x = f'x{str(i).zfill(2)}'
+        y = f'y{str(i).zfill(2)}'
+        wires[x] = 0
+        wires[y] = 0
+
+def get_x(wires):
+    number = ''
+    for wire in sorted(wires):
+        if wire.startswith('x'):
+            number += str(wires[wire])
+    number = '0b' + number[::-1]
+    return number, int(number, 2)
+
+def get_y(wires):
+    number = ''
+    for wire in sorted(wires):
+        if wire.startswith('y'):
+            number += str(wires[wire])
+    number = '0b' + number[::-1]
+    return number, int(number, 2)
+
+def setx(id, val):
+    x = f'x{str(id).zfill(2)}'
+    wires[x] = val
+
+def sety(id, val):
+    y = f'y{str(id).zfill(2)}'
+    wires[y] = val
+
+### This was used to check how subsequent bits are handled...
+
+wires, gates, exec_queue, wire_to_gate = parse(data)
+#render()
+reset(wires)
+setx(0, 1)
+sety(0, 1)
+run_gates(wires, exec_queue, wire_to_gate)
+print(get_x(wires))
+print(get_y(wires))
+print('-----')
+print(get_z(wires))
+print(get_x(wires)[1] + get_y(wires)[1] == get_z(wires)[1])
+
+### And finally the solution that I discovered by analyzing the rendered graph
+
+def repair_gates(gates):
+    gates[1].out = 'gpr'
+    gates[59].out = 'z10'
+    gates[111].out = 'nks'
+    gates[7].out = 'z21'
+    gates[109].out = 'ghp'
+    gates[2].out = 'z33'
+    gates[24].out = 'krs'
+    gates[91].out = 'cpm'
+    print(','.join(sorted(list(gates[i].out for i in (1, 59, 7, 111, 109, 2, 24, 91)))))
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ For the seventh time tried to find out how far I can make it in [Advent of Code]
 * [2022](https://github.com/Leftfish/Advent-of-Code-2022): 25 days for the third time!
 * [2023](https://github.com/Leftfish/Advent-of-Code-2023): 25 days for the fourth time!
 
-Not only am I (still) not a pro, nor do I have a degree in computer science, but also this is a pretty challenging time for me. As of Day 25, I managed to solve all the problems on the same day with two exceptions - Days 21 and Day 24 part 2. Their difficulty, as well as time constraints, caused me to abandon both for now. I will be back!
+Not only am I (still) not a pro, nor do I have a degree in computer science, but also this is a pretty challenging time for me. As of Day 25, I managed to solve all the problems on the day of their release with two exceptions - Days 21 and Day 24 part 2. I finished Day 24 a day too late, mostly due to time constraints. Day 21 is just very difficult for me, and as far as I know, I'm not the only one who struggles with it. I do have an idea how to approach it, but time is not unlimited, so it has to wait a bit longer. On the whole, 47 stars on Day 25 is probably my best result ever in AoC.
 
 Although I'm not done yet, I can already say that it was a pretty good year in terms of not having to rely on suggestions, tutorials and other solutions. For now, I needed that twice. [Day 16 part 2](https://adventofcode.com/2024/day/16) was the first one that required me to have a look at the solutions posted by the [AoC community on reddit](https://www.reddit.com/r/adventofcode/) - it turned out that I had the right ideas and terrible approaches to implementation. Another one was Day 20 part 2 - I think I was just tired, because I ended up solving the puzzle after applying an idea that I'd already used this year, but needed a nudge from Reddit to return to it. 
 
@@ -37,5 +37,5 @@ Things I **L**earned, **R**evised or **I**mproved at in 2024:
 * Day 21: TO DO
 * [Day 22 Python](22/d22.py): bitwise operations (**R**) and using [xorshift](https://en.wikipedia.org/wiki/Xorshift) to generate pseudorandom numbers (**L**)
 * [Day 23 Python](23/d23.py): [clique problem](https://en.wikipedia.org/wiki/Clique_problem) (**L**) and networkx (**I**)
-* [Day 24 Python](24/d24.py): logic gates (**R**), PART 2 TO DO
+* [Day 24 Python](24/d24.py): logic gates (**R**), adders (**I**) and ripple-carry adders (**L**), graphviz (**I**)
 * [Day 25 Python](25/d25.py): list comprehensions (**R**) and itertools.product (**R**)
