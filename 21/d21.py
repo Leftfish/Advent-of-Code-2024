b34b4540a711c932428052879333a7af712b51d3
Leftfish <bohdan@widla.net>
2024-12-26 13:21:01 +0100

    Add more code to unfinished Day 21.

--- 21/d21.py
+++ 21/d21.py
@@ -84,7 +84,6 @@ KEYPAD_DIRECTIONS = {('^', 'A'): R,
 directions = ((-1, 0), (1, 0), (0, -1), (0, 1), (0, 0))
 
 def get_paths(keyboard, keyboard_directions):
-
     grd = list(line for line in keyboard.splitlines())
     keyboard_graph = {}
     graph = nx.Graph()
@@ -123,20 +122,101 @@ def get_paths(keyboard, keyboard_directions):
 
 numpad_paths = get_paths(NUMPAD, NUMPAD_DIRECTIONS)
 keypad_paths = get_paths(KEYPAD, KEYPAD_DIRECTIONS)
-
-
-
-to_write = list('v<<A>>^A<A>AvA<^AA>A<vAAA>^A')
+to_write = list('029A')
 steps = list(zip(to_write, to_write[1:]))
 current = ENTER
 nxt = steps[0][0]
 steps.insert(0, (current, nxt))
-
 total = ''
 for pair in steps:
-    total += keypad_paths[pair]
+    total += numpad_paths[pair]
     total += ENTER
 print(total)
+
+
+
+print('-----')
+
+def get_shortest_paths(keyboard):
+    grd = list(line for line in keyboard.splitlines())
+    keyboard_graph = {}
+    graph = nx.Graph()
+    for i, line in enumerate(grd):
+        for j, c in enumerate(line):
+            if c != '.':
+                keyboard_graph[(i, j)] = c
+                graph.add_node(c)
+
+    for k, v in keyboard_graph.items():
+        i, j = k
+        for direction in directions:
+            di, dj = direction
+            adj = (i+di, j+dj)
+            if adj in keyboard_graph:
+                graph.add_edge(v, keyboard_graph[adj])
+
+    node_to_node = product(graph.nodes, graph.nodes)
+    paths = {}
+    for pair in node_to_node:
+        start, end = pair
+        paths[(start, end)] = list(nx.all_shortest_paths(graph, start, end))
+        paths[(end, start)] = list(nx.all_shortest_paths(graph, end, start))
+    return paths
+
+def num_to_arrows(current, target, numpad_paths):
+    paths = []
+    candidates = numpad_paths[(current, target)]
+    for c in candidates:
+        s = ''
+        steps = zip(c, c[1:])
+        for step in steps:
+            s += NUMPAD_DIRECTIONS[step]
+        s += ENTER
+        paths.append(s)
+    return paths
+
+def arrows_to_arrows(current, target, keypad_paths):
+    paths = []
+    candidates = keypad_paths[(current, target)]
+    for c in candidates:
+        s = ''
+        steps = zip(c, c[1:])
+        for step in steps:
+            s += KEYPAD_DIRECTIONS[step]
+        s += ENTER
+        paths.append(s)
+    return paths
+
+
+keypad_paths = get_shortest_paths(KEYPAD)
+numpad_paths = get_shortest_paths(NUMPAD)
+
+print(num_to_arrows('A', '0', numpad_paths))  # przykład, pierwszy ruch na numerycznej przełożony na strzałki (robot startuje na A)
+print(arrows_to_arrows('A', '<', keypad_paths)) # przykład, pierwszy ruch na strzałkach przełożony na strzałki (robot startuje na A)
+print(arrows_to_arrows('A', 'v', keypad_paths)) # przykład, pierwszy ruch na drugich strzałkach przełożony na strzałki (robot startuje na A)
+print('####')
+print(num_to_arrows('0', '2', numpad_paths))   # przykład, drugi ruch na numerycznej przełożony na strzałki (robot startuje na 0 bo tam był)
+print(arrows_to_arrows('<', 'A', keypad_paths)) # przykład, drugi ruch na strzałkach przełożony na strzałki (robot startuje na < i wbija do A)
+print(arrows_to_arrows('v', '<', keypad_paths)) # przykład, drugi ruch na drugich strzałkach przełożony na strzałki (robot startuje na v i leci do <)
+
+## TO TERAZ CHYBA TRZEBA TAK
+## dla każdej pary na numerycznej wygeneruj shortest path
+## dla każdej shortest path wygeneruj dla niej shortest path na strzałkach
+## dla każdej tak wygenerowanej wygeneruj dla niej shortest path na strzałkach
+## weź najkrótszą, doczep do wyniku
+
+
+## zrób najkrótsze ścieżki na numerycznej
+## zrób najkrótsze ścieżki na strzałkach
+## weź najkrótsze ścieżki od cyfry do cyfry na numerycznej, każdą z tych ścieżek przełóż na strzałki (to robi num_to_arrows)
+## weź najkrótsze ścieżki od strzałki do strzałki, każdą przełóż na strzałki (to robi arrows_to_arrows)
+
+## ruch z node do node na numerycznej
+## iteruj przez ścieżki numeric przełożone na strzałki, każdą przełóż na strzałki, każdą przełóż na strzałki
+## weź najkrótszą
+
+
+
 
 ### przekładanie ruchów na pady - działa bdb, te z przykładu przekłada jak trzeba
 ### brakuje optymalizacji (nie ma jednak ekwiwalentu między ścieżkami i Manhattan distance - to jak polecisz na pierwszym etapie skutkuje innymi ruchami na dalszych)
